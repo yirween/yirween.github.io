9fdce13212fe0f596657175f5bc2bcb84f4248e6
yirween <110904987+yirween@users.noreply.github.com>
2023-09-23 14:14:30 +0800

    Add files via upload

--- JS/change-background.js
+++ JS/change-background.js
@@ -1,4 +1,23 @@
 
+function setBackgroundImage() {
+    var bgImage = getCookie("bgImage");
+    if (bgImage) {
+        var img = document.querySelector('.beijing img');
+        img.setAttribute('src', bgImage);
+    }
+}
+
+function getCookie(name) {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.indexOf(name + '=') === 0) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
+}
+
 function bg1(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','./Sprite/warma-boom8 .jpg')
@@ -7,110 +26,110 @@ function bg1(){
 function bg2(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/10/pPc8dcd.jpg')
-    document.cookie = "bgImage=./Sprite/warma-boom8 .jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/10/pPc8dcd.jpg; path=/";
 }
 function bg3(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/10/pPc8U9e.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/10/pPc8U9e.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/10/pPc8U9e.jpg; path=/";
 }
 function bg4(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/10/pPc8a1H.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/10/pPc8a1H.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/10/pPc8a1H.jpg; path=/";
 }
 function bg5(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcn1g0.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcn1g0.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcn1g0.jpg; path=/";
 }
 function bg6(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcmzHe.jpg')
-    document.cookie = "//s1.ax1x.com/2023/09/09/pPcmzHe.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmzHe.jpg; path=/";
 }
 function bg7(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/10/pPc8wjA.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/10/pPc8wjA.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/10/pPc8wjA.jpg; path=/";
 }
 function bg8(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcnpAH.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcnpAH.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcnpAH.jpg; path=/";
 }
 function bg9(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcmXjK.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmXjK.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmXjK.jpg; path=/";
 }
 function bg10(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcmvnO.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmvnO.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmvnO.jpg; path=/";
 }
 function bg11(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcmOc6.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmOc6.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmOc6.jpg; path=/";
 }
 function bg12(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcmxBD.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmxBD.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmxBD.jpg; path=/";
 }
 function bg13(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcm7N9.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcm7N9.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcm7N9.jpg; path=/";
 }
 function bg14(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcmL1x.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmL1x.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmL1x.jpg; path=/";
 }
 function bg15(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcmq91.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmq91.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmq91.jpg; path=/";
 }
 function bg16(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcmHhR.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmHhR.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmHhR.jpg; path=/";
 }
 function bg17(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcm5BF.jpg')
-    document.cookie = "//s1.ax1x.com/2023/09/09/pPcm5BF.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcm5BF.jpg; path=/";
 }
 function bg18(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcmI74.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmI74.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmI74.jpg; path=/";
 }
 function bg19(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcm4nU.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcm4nU.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcm4nU.jpg; path=/";
 }
 function bg20(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcmTAJ.jpg')
-    document.cookie = "//s1.ax1x.com/2023/09/09/pPcmTAJ.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmTAJ.jpg; path=/";
 }
 function bg21(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//s1.ax1x.com/2023/09/09/pPcmfXT.jpg')
-    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmfXT.jpg";
+    document.cookie = "bgImage=//s1.ax1x.com/2023/09/09/pPcmfXT.jpg; path=/";
 }
 function bg22(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//z1.ax1x.com/2023/09/15/pPW2omd.jpg')
-    document.cookie = "bgImage=//z1.ax1x.com/2023/09/15/pPW2omd.jpg";
+    document.cookie = "bgImage=//z1.ax1x.com/2023/09/15/pPW2omd.jpg; path=/";
 }//星辰点亮了前进的道路
 function bg23(){
     var img = document.querySelector('.beijing img')
     img.setAttribute('src','//z1.ax1x.com/2023/09/22/pPoB9pT.jpg')
-    document.cookie = "bgImage=//z1.ax1x.com/2023/09/22/pPoB9pT.jpg";
+    document.cookie = "bgImage=//z1.ax1x.com/2023/09/22/pPoB9pT.jpg; path=/";
 }//晨光——通往秋天
