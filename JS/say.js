a0740470ffb0b8516044883812a1ef46944699f6
yirween <110904987+yirween@users.noreply.github.com>
2024-11-07 20:57:10 +0800

    Add files via upload

---
+++ JS/say.js
@@ -0,0 +1,45 @@
+const stringsList = [
+    "你也要过饭?", 
+    "偶尔看看脚下的风景不也挺好吗", 
+    "看我干嘛?", "哇!是活人哎!",
+    "我已经完全爱上沃玛了！", 
+    "海内存知己，天涯若比邻",
+    "衬衫的价格是九磅十五便士",
+    "在满地都是六便士的街上，他抬起头看到了月亮。‌",
+    "圣人不死，大盗不至",
+    "再一次谈到自己的年龄，是「我才」还是「我已经」",
+    "When the night begins",
+    "言语的悲伤就忘了吧",
+    "我最后还是知道了方法 继续面对吧",
+    "不停地追逐着温柔月光 奔跑下去吧",
+    "你是这世界上独一无二的光",
+    "潮汐来时 注定抹去",
+    
+    ];
+
+// 获取<div>元素
+const saysDiv = document.querySelector('.says');
+
+// 定义一个函数来执行动画和文本更新
+function updateDiv() {
+  // 将opacity在一秒内递减到0
+  saysDiv.style.opacity = '0';
+  
+  // 使用setTimeout等待一秒（1000毫秒）以确保opacity过渡完成
+  setTimeout(() => {
+    // 随机选择list中的一个字符串
+    const randomString = stringsList[Math.floor(Math.random() * stringsList.length)];
+    
+    // 修改<div>的文本内容
+    saysDiv.textContent = randomString;
+    
+    // 将opacity在一秒内递增到1
+    saysDiv.style.opacity = '1';
+    
+    // 再次等待五秒然后重复整个过程
+    setTimeout(updateDiv, 5000);
+  }, 1000); // 与CSS中的transition时间相匹配
+}
+
+// 初始调用函数来开始循环
+updateDiv();
